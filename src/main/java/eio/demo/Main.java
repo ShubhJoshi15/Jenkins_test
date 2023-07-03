b8853b3842131233dd658399bb29e6db8740c9a6
Shubham <shubhjoshi1595@gmail.com>
2023-07-03 15:12:46 +0530

    Adding excel read logic to Main class Adding dependency in pom

--- src/main/java/eio/demo/Main.java
+++ src/main/java/eio/demo/Main.java
@@ -1,7 +1,31 @@
 package eio.demo;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("D:\\Quarkus\\employee.xlsx");
+        FileInputStream fis = new FileInputStream(file);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        System.out.println();
+
+        for(Row row : sheet)
+        {
+            for(Cell cell : row)
+            {
+                System.out.print(cell.toString()+ " \t"+" \t ");
+            }
+            System.out.println();
+        }
     }
 }
