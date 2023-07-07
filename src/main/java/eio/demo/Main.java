439e4254cc97a0340203db729695b21215209d3f
Shubham <shubhjoshi1595@gmail.com>
2023-07-07 15:11:46 +0530

    new logic to read multiple file which is stored in workspace

--- src/main/java/eio/demo/Main.java
+++ src/main/java/eio/demo/Main.java
@@ -3,42 +3,86 @@ package eio.demo;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args)  {
+        // Temporary comment for another logic
+
+//        try {
+//           // if(args.length>0) {
+//               // String FileName = args[0];
+//                String Name = "../ICD_Data.xlsx";
+//               // String FilePath = args[1];
+//               // String FinalFileName = FilePath.concat("").concat(FileName);
+//                //System.out.println("File Name with path is :-"+FinalFileName);
+//                //System.out.println();
+//
+//               // File file = new File(FinalFileName);
+//                FileInputStream fis = new FileInputStream(Name);
+//                XSSFWorkbook workbook = new XSSFWorkbook(fis);
+//                Sheet sheet = workbook.getSheetAt(0);
+//
+//                System.out.println("Data IS -> \n");
+//
+//                for (Row row : sheet) {
+//                    for (Cell cell : row) {
+//                        System.out.print(cell.toString() + " \t" + " \t ");
+//                    }
+//                    System.out.println();
+//                }
+//            //}
+//
+//
+//            System.out.println("Hello Shubham....");
+//        }catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+
+
         try {
-           // if(args.length>0) {
-               // String FileName = args[0];
-                String Name = "../ICD_Data.xlsx";
-               // String FilePath = args[1];
-               // String FinalFileName = FilePath.concat("").concat(FileName);
-                //System.out.println("File Name with path is :-"+FinalFileName);
-                //System.out.println();
 
-               // File file = new File(FinalFileName);
-                FileInputStream fis = new FileInputStream(Name);
-                XSSFWorkbook workbook = new XSSFWorkbook(fis);
-                Sheet sheet = workbook.getSheetAt(0);
+            String folderPath = "../";
+            File folder = new File(folderPath);
+            File[] listOfFiles = folder.listFiles();
 
-                System.out.println("Data IS -> \n");
-
-                for (Row row : sheet) {
-                    for (Cell cell : row) {
-                        System.out.print(cell.toString() + " \t" + " \t ");
-                    }
-                    System.out.println();
+            for(File f : listOfFiles)
+            {
+                if(f.exists())
+                {
+                    if(f.getName().endsWith(".xlsx")){
+                    System.out.println("File present -> "+f.getName());}
                 }
-            //}
+                else {
+                    System.out.println("File not present reassign with new name");
+                    listOfFiles = new File(f.getName().substring(3)).listFiles();
+                }
+            }
 
-
-            System.out.println("Hello Shubham....");
-        }catch (Exception e)
-        {
+            for (File file : listOfFiles) {
+                if (file.isFile() && file.getName().endsWith(".xlsx")) {
+                        System.out.println("The Excel file is : ->"+file.getName()+"\n");
+                        FileInputStream fis = new FileInputStream(file);
+                        Workbook workbook = new XSSFWorkbook(fis);
+                        Sheet sheet = workbook.getSheetAt(0);
+                        for (Row row : sheet) {
+                            for (Cell cell : row) {
+                                System.out.print(cell.toString() + " \t" + " \t ");
+                            }
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+            }
+            }catch (IOException e)
+            {
             e.printStackTrace();
-        }
+            }
     }
 }
