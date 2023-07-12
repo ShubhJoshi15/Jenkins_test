ff518a9277fcceb7d9a7ce99d766693eab7ed123
Shubham <shubhjoshi1595@gmail.com>
2023-07-12 12:44:11 +0530

    new logic to read multiple file which is stored in workspace from kp

--- src/main/java/eio/demo/Main.java
+++ src/main/java/eio/demo/Main.java
@@ -98,7 +98,7 @@ public class Main {
                 }
                 else {
                     System.out.println("File not exist reassign with different file name ");
-                  //  fileName = "../"+fileName;
+                    fileName = "../"+fileName;
 
                 }
                 FileInputStream fis = new FileInputStream(fileName);
