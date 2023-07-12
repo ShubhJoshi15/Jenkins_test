d0fe047686ce59ef5c11277f728b2c17383a0f6d
Shubham <shubhjoshi1595@gmail.com>
2023-07-12 12:08:46 +0530

    new logic to read multiple file which is stored in workspace from kp

--- src/main/java/eio/demo/Main.java
+++ src/main/java/eio/demo/Main.java
@@ -46,43 +46,79 @@ public class Main {
 //        }
 
 
+//        try {
+//
+//            String folderPath = "../";
+//            File folder = new File(folderPath);
+//            File[] listOfFiles = folder.listFiles();
+//
+//            for(File f : listOfFiles)
+//            {
+//                if(f.exists())
+//                {
+//                    if(f.getName().endsWith(".xlsx")){
+//                    System.out.println("File present -> "+f.getName());}
+//                }
+//                else {
+//                    System.out.println("File not present reassign with new name");
+//                    listOfFiles = new File(f.getName().substring(3)).listFiles();
+//                }
+//            }
+//
+//            for (File file : listOfFiles) {
+//                if (file.isFile() && file.getName().endsWith(".xlsx")) {
+//                        System.out.println("The Excel file is : ->"+file.getName()+"\n");
+//                        FileInputStream fis = new FileInputStream(file);
+//                        Workbook workbook = new XSSFWorkbook(fis);
+//                        Sheet sheet = workbook.getSheetAt(0);
+//                        for (Row row : sheet) {
+//                            for (Cell cell : row) {
+//                                System.out.print(cell.toString() + " \t" + " \t ");
+//                            }
+//                        System.out.println();
+//                    }
+//                }
+//                System.out.println();
+//            }
+//            }catch (IOException e)
+//            {
+//            e.printStackTrace();
+//            }
+
         try {
-
-            String folderPath = "../";
-            File folder = new File(folderPath);
-            File[] listOfFiles = folder.listFiles();
-
-            for(File f : listOfFiles)
+            for(String fileArg : args)
             {
-                if(f.exists())
-                {
-                    if(f.getName().endsWith(".xlsx")){
-                    System.out.println("File present -> "+f.getName());}
+                //fileList.add(fileArg);
+                String fileName = fileArg;
+                File f = new File(fileName);
+
+                if (f.exists()) {
+                    System.out.println("Exists");
                 }
                 else {
-                    System.out.println("File not present reassign with new name");
-                    listOfFiles = new File(f.getName().substring(3)).listFiles();
+                    System.out.println("File not exist reassign with different file name ");
+                    fileName = "../"+fileName;
+
+                }
+                FileInputStream fis = new FileInputStream(fileName);
+                XSSFWorkbook workbook = new XSSFWorkbook(fis);
+                Sheet sheet = workbook.getSheetAt(0);
+
+                System.out.println("Data IS -> \n");
+
+                for (Row row : sheet) {
+                    for (Cell cell : row) {
+                        System.out.print(cell.toString() + " \t" + " \t ");
+                    }
+                    System.out.println();
                 }
             }
 
-            for (File file : listOfFiles) {
-                if (file.isFile() && file.getName().endsWith(".xlsx")) {
-                        System.out.println("The Excel file is : ->"+file.getName()+"\n");
-                        FileInputStream fis = new FileInputStream(file);
-                        Workbook workbook = new XSSFWorkbook(fis);
-                        Sheet sheet = workbook.getSheetAt(0);
-                        for (Row row : sheet) {
-                            for (Cell cell : row) {
-                                System.out.print(cell.toString() + " \t" + " \t ");
-                            }
-                        System.out.println();
-                    }
-                }
-                System.out.println();
-            }
-            }catch (IOException e)
-            {
+            System.out.println("Hello EIO Team....");
+
+        }catch (Exception e)
+        {
             e.printStackTrace();
-            }
+        }
     }
 }
